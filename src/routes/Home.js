cd537b747a3bf17a9c9dbfc8c7eb7cb5b927e74f
sihyonn <sihyeon982@naver.com>
2023-09-05 15:14:46 +0900

    feat: 기본 라우터 구성

---
+++ src/routes/Home.js
@@ -0,0 +1,7 @@
+import { Component } from "../core/sihyonn";
+
+export default class Home extends Component {
+  render() {
+    this.el.innerHTML = "<h1>안녕 홈!</h1>";
+  }
+}
---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+import { createRouter } from "../core/sihyonn";
+import Home from "./Home";
+
+export default createRouter([
+  {
+    path: "#/",
+    component: Home,
+  },
+]);
